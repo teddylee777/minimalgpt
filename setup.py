baaad66f57e8ad95d3394887b8bed8952792caaa
teddylee777 <teddylee777@gmail.com>
2023-11-18 01:43:39 +0900

    설치 버그 fix, minor bug fix, 데모파일 업데이트

--- setup.py
+++ setup.py
@@ -2,19 +2,19 @@ from setuptools import setup, find_packages
 
 setup(
     name='minimalgpt',
-    version='0.0.5',
+    version='0.0.9',
     description='Simple LangChain Wrapper Package for ease of using ChatGPT',
     author='teddylee777',
     author_email='teddylee777@gmail.com',
     url='https://github.com/teddylee777/minimalgpt',
     install_requires=[
         'beautifulsoup4==4.12.2', 
-        'pandas==1.5.3',
         'tabulate==0.9.0',
-        'openai==0.29.1', 
+        'openai==0.28.1', 
         'langchain==0.0.336', 
         'tiktoken==0.5.1', 
-        'langchain-experimental-0.0.41'
+        'langchain-experimental==0.0.41',
+        'pandas==1.5.3',
     ],
     packages=find_packages(exclude=[]),
     keywords=['minimalgpt', 'teddylee777', 'chatgpt', 'langchain', 'pandasai'],
